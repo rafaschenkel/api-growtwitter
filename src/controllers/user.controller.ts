0f586cf22f9972e157a1c19d76cd1d188f6cc1fc
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-05-27 17:33:10 -0300

    feat user controller implement create, getUser, listFollowings and listFollowers with error handling

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import userService from "../services/user.service";
+import User from "../model/user.model";
+import { handlerError } from "../utils/handlerError.utils";
+class UserController {
+  public async create(req: Request, res: Response) {
+    try {
+      const { username, email, password } = req.body;
+      const user = new User(username, email, password);
+      const result = await userService.create(user);
+
+      res.status(result.code).send(result);
+    } catch (error: any) {
+      handlerError(error, res);
+    }
+  }
+
+  public async getUser(req: Request, res: Response) {
+    try {
+      const { userId } = req.params;
+      const result = await userService.getUser(userId);
+
+      res.status(result.code).send(result);
+    } catch (error: any) {
+      handlerError(error, res);
+    }
+  }
+
+  public async listFollowings(req: Request, res: Response) {
+    try {
+      const { userId } = req.params;
+      const result = await userService.listFollowings(userId);
+      res.status(result.code).send(result);
+    } catch (error: any) {
+      handlerError(error, res);
+    }
+  }
+
+  public async listFollowers(req: Request, res: Response) {
+    try {
+      const { userId } = req.params;
+      const result = await userService.listFollowers(userId);
+      res.status(result.code).send(result);
+    } catch (error: any) {
+      handlerError(error, res);
+    }
+  }
+}
+
+export default new UserController();
