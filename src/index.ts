f0a3b60f26d03f53dd4534f93950d84bb2a36822
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-05-27 17:37:07 -0300

    feat: configure Express with CORS, dotenv, and user routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotEnv from "dotenv";
+import userRoutes from "./routes/user.route";
 
 dotEnv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/user", userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
