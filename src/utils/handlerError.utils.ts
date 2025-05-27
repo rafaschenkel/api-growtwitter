41ae35f766c63c90663cee07736a53bd94ef1c65
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-05-27 17:27:45 -0300

    fix: add centralized error handler for Prisma and custom AppError with proper status codes

---
+++ src/utils/handlerError.utils.ts
@@ -0,0 +1,31 @@
+import { Prisma } from "@prisma/client";
+import { Response } from "express";
+import { AppError } from "../model/userError";
+
+export function handlerError(error: Error, res: Response) {
+  let result;
+
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    result = {
+      code: Number(error.code),
+      message: error.message,
+    };
+  } else if (error instanceof Prisma.PrismaClientValidationError) {
+    result = {
+      code: 400,
+      message: error.message,
+    };
+  } else if (error instanceof AppError) {
+    result = {
+      code: error.code,
+      message: error.message,
+    };
+  } else {
+    result = {
+      code: 500,
+      message: error.message,
+    };
+  }
+
+  return res.status(result.code).send(result);
+}
