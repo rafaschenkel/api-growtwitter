920067e2f08cd27027eb0c82086f6621a3b7dd60
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-05-28 19:32:50 -0300

    feat: add login, follow, unfollow and followingTweets routes with auth middleware

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const userRoutes = Router();
 
 userRoutes.post("/", userController.create);
-userRoutes.get("/search/:userId", userController.getUser);
-userRoutes.get("/followings/:userId", userController.listFollowings);
-userRoutes.get("/followers/:userId", userController.listFollowers);
+userRoutes.post("/login", userController.login);
+userRoutes.get("/followingTweets", authMiddleware, userController.listFollowingTweets);
+
+userRoutes.get("/:userId", userController.getUser);
+userRoutes.get("/:userId/followings", userController.listFollowings);
+userRoutes.get("/:userId/followers", userController.listFollowers);
+
+userRoutes.post("/follow/:followingId", authMiddleware, userController.follow);
+userRoutes.delete("/unfollow/:followingId", authMiddleware, userController.unfollow);
 
 export default userRoutes;
