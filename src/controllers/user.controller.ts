efd0b228f7c7fe4f2f5e761306a3ac9745abc929
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-05-28 19:31:59 -0300

    feat: implement login with JWT and list tweets from followings

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,7 +3,18 @@ import userService from "../services/user.service";
 import User from "../model/user.model";
 import { handlerError } from "../utils/handlerError.utils";
 import { CreateUserDto } from "../dtos/createUser.dto";
+import { LoginDto } from "../dtos/login.dto";
 class UserController {
+  public async login(req: Request, res: Response) {
+    try {
+      const { login, password } = req.body;
+      const loginData: LoginDto = { login, password };
+      const result = await userService.login(loginData);
+      res.status(result.code).send(result);
+    } catch (error: any) {
+      handlerError(error, res);
+    }
+  }
   public async create(req: Request, res: Response) {
     try {
       const { username, email, password } = req.body;
@@ -40,6 +51,38 @@ class UserController {
     try {
       const { userId } = req.params;
       const result = await userService.listFollowers(userId);
+      res.status(result.code).send(result);
+    } catch (error: any) {
+      handlerError(error, res);
+    }
+  }
+
+  public async listFollowingTweets(req: Request, res: Response) {
+    try {
+      const userId = req.userId;
+      const result = await userService.listFollowingTweets(userId);
+      res.status(result.code).send(result);
+    } catch (error: any) {
+      handlerError(error, res);
+    }
+  }
+
+  public async follow(req: Request, res: Response) {
+    try {
+      const userId = req.userId;
+      const { followingId } = req.params;
+      const result = await userService.follow(userId, followingId);
+      res.status(result.code).send(result);
+    } catch (error: any) {
+      handlerError(error, res);
+    }
+  }
+
+  public async unfollow(req: Request, res: Response) {
+    try {
+      const userId = req.userId;
+      const { followingId } = req.params;
+      const result = await userService.unfollow(userId, followingId);
       res.status(result.code).send(result);
     } catch (error: any) {
       handlerError(error, res);
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,8 +6,33 @@ import User from "../model/user.model";
 import { AppError } from "../model/appError";
 import { CreateUserDto } from "../dtos/createUser.dto";
 import { hashPassword } from "../utils/hashPassword.utils";
+import jwt, { SignOptions } from "jsonwebtoken";
+import { comparePassword } from "../utils/comparePassword.utils";
+import { LoginDto, LoginResponseDto } from "../dtos/login.dto";
+import { TweetDto } from "../dtos/tweet.dto";
 
 class UserService {
+  public async login(data: LoginDto): Promise<ResultDto<LoginResponseDto>> {
+    const user = await repository.user.findFirst({
+      where: { OR: [{ email: data.login }, { username: data.login }] },
+    });
+    if (!user) throw new AppError(401, "Invalid email/username or password credentials");
+
+    const passwordvalid = await comparePassword(data.password, user.password);
+    if (!passwordvalid) throw new AppError(401, "Invalid email/username or password credentials");
+
+    const signOptions = { expiresIn: process.env.JWT_EXPIRES_IN } as SignOptions;
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, {
+      expiresIn: signOptions.expiresIn,
+    });
+
+    const { password, ...userWithoutPassword } = user;
+    return {
+      code: 200,
+      message: "User successfully logged in",
+      data: { token, user: userWithoutPassword },
+    };
+  }
   public async create(data: CreateUserDto): Promise<ResultDto<UserDto>> {
     const hashedPassword = await hashPassword(data.password);
     const user = new User(data.username, data.email, hashedPassword);
@@ -56,7 +81,11 @@ class UserService {
   public async listFollowers(userId: string): Promise<ResultDto<FollowerDto[]>> {
     const result = await repository.follow.findMany({
       where: { followingId: userId },
-      select: { follower: { select: { id: true, username: true } } },
+      select: {
+        follower: {
+          select: { id: true, username: true, _count: { select: { following: true, followers: true, tweets: true } } },
+        },
+      },
     });
 
     if (result.length === 0) throw new AppError(404, "This user has no followers");
@@ -65,6 +94,54 @@ class UserService {
       code: 200,
       message: "Followers successfully retrieved",
       data: result,
+    };
+  }
+
+  public async listFollowingTweets(userId: string): Promise<ResultDto<TweetDto[]>> {
+    const result = await repository.follow.findMany({
+      where: { followerId: userId },
+      select: { following: { select: { tweets: { include: { user: { select: { id: true, username: true } } } } } } },
+    });
+
+    const tweets = result.flatMap((follow) => follow.following.tweets);
+
+    return {
+      code: 200,
+      message: "Following tweets successfully retrieved",
+      data: tweets,
+    };
+  }
+
+  public async follow(userId: string, followingId: string) {
+    const existingFollow = await repository.follow.findUnique({
+      where: { followerId_followingId: { followerId: userId, followingId: followingId } },
+    });
+
+    if (existingFollow) throw new AppError(400, "User already followed");
+
+    await repository.follow.create({
+      data: { followerId: userId, followingId: followingId },
+    });
+    return {
+      code: 200,
+      message: "User successfully followed",
+    };
+  }
+
+  public async unfollow(userId: string, followingId: string) {
+    const existingFollow = await repository.follow.findUnique({
+      where: { followerId_followingId: { followerId: userId, followingId: followingId } },
+    });
+
+    if (!existingFollow) throw new AppError(400, "User not followed");
+
+    await repository.follow.deleteMany({
+      where: { followerId: userId, followingId: followingId },
+    });
+
+    return {
+      code: 200,
+      message: "User successfully unfollowed",
     };
   }
 }
