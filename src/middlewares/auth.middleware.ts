006c2cee15fabd86728d2b68cc7a1d4ae5f8f3b2
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-05-30 16:44:50 -0300

    feat: enhance authMiddleware to validate token format and verify user existence in DB

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,17 +1,18 @@
 import jwt from "jsonwebtoken";
-import express, { NextFunction, Response } from "express";
-import { AppError } from "../model/appError";
+import express, { NextFunction, Request, Response } from "express";
+import { AppError } from "../model/appError.model";
 import { handlerError } from "../utils/handlerError.utils";
+import { repository } from "../database/prismaClient";
 
 interface Payload extends jwt.JwtPayload {
-  userId: string;
+  userId?: string;
 }
 
-export async function authMiddleware(req: express.Request, res: Response, next: NextFunction) {
+export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
   try {
     const { authorization } = req.headers;
 
-    if (!authorization) throw new AppError(401, "Token not found");
+    if (!authorization || !authorization.startsWith("Bearer ")) throw new AppError(401, "Token not found");
 
     const token = authorization.replace("Bearer ", "");
 
@@ -19,7 +20,12 @@ export async function authMiddleware(req: express.Request, res: Response, next: 
 
     const { userId } = jwt.verify(token, process.env.JWT_SECRET as string) as Payload;
 
-    req.userId = userId;
+    if (!userId) throw new AppError(401, "Invalid token");
+
+    const userExists = await repository.user.findUnique({ where: { id: userId } });
+    if (!userExists) throw new AppError(404, "User not found or does not exist");
+
+    req.userId = userExists.id;
 
     next();
   } catch (error: any) {
