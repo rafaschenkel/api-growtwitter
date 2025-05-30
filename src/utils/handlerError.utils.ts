b8987fbd366dabb84a1302011fdf6923562efb40
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-05-30 16:57:54 -0300

    refactor: standardize error response using ResultDto and include success flag

--- src/utils/handlerError.utils.ts
+++ src/utils/handlerError.utils.ts
@@ -1,27 +1,32 @@
 import { Prisma } from "@prisma/client";
 import { Response } from "express";
-import { AppError } from "../model/appError";
+import { AppError } from "../model/appError.model";
+import { ResultDto } from "../dtos/result.dto";
 
-export function handlerError(error: Error, res: Response) {
-  let result;
+export function handlerError(error: Error, res: Response): Response<ResultDto> {
+  let result: ResultDto;
 
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
     result = {
+      success: false,
       code: 500,
       message: error.message,
     };
   } else if (error instanceof Prisma.PrismaClientValidationError) {
     result = {
+      success: false,
       code: 400,
       message: error.message,
     };
   } else if (error instanceof AppError) {
     result = {
+      success: false,
       code: error.code,
       message: error.message,
     };
   } else {
     result = {
+      success: false,
       code: 500,
       message: error.message,
     };
