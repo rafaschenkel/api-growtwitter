fcad77b1ddb3c62ee7e28205fd6345d8da068995
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-05-30 16:41:25 -0300

    chore: add tweetRoutes and validateJsonSyntax middleware to app

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotEnv from "dotenv";
 import userRoutes from "./routes/user.route";
+import tweetRoutes from "./routes/tweet.routes";
+import { validateJsonSyntax } from "./middlewares/validateJsonSyntax.middleware";
+import { checkBodyEmptyMiddleware } from "./middlewares/checkBodyEmpty.middleware";
 
 dotEnv.config();
 
@@ -9,7 +12,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(validateJsonSyntax);
 app.use("/user", userRoutes);
+app.use("/tweet", tweetRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
